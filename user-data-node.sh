5d66dd059d335718e22f24dddd1023cdbb554b04
wultur <wultur@gmail.com>
2022-03-21 09:36:55 +0300

    node_exporter service user added

--- user-data-node.sh
+++ user-data-node.sh
@@ -3,8 +3,9 @@ wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_e
 tar xvfz node_exporter-1.3.1.linux-amd64.tar.gz
 sudo mv node_exporter-1.3.1.linux-amd64/node_exporter /usr/local/bin/
 sudo rm -rf node_exporter*
-git clone https://github.com/Wultur/prometheus-25.git
-sudo cp prometheus-25/node_exporter/node_exporter.service /etc/systemd/system/node_exporter.service
+sudo git clone https://github.com/Wultur/prometheus-25.git
+sudo cp prometheus-25/node-exporter/node_exporter.service /etc/systemd/system
+sudo useradd --system --no-create-home --shell /bin/false node_exporter
 sudo rm -rf prometheus-25
 sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
